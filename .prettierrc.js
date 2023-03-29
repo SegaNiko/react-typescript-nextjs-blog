765fd3550436e1b565ab411cac8089e993207d65
SegaNiko <sergiynikolaiychuk@gmail.com>
2023-03-29 13:36:45 +0300

    tech: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,11 @@ module.exports = {
   semi: true,
   trailingComma: 'all',
   singleQuote: true,
+  jsxSingleQuote: true,
   printWidth: 120,
   tabWidth: 2,
   bracketSpacing: true,
+  jsxBracketSameLine: true,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
 };
